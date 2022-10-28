ddab2121aee71880ee83c4ca9b87ff2f5c849535
giahuy2002 <78332589+giahuy2002@users.noreply.github.com>
2022-10-28 18:48:16 +0700

    Add page and styles

---
+++ pages/misspassword.js
@@ -0,0 +1,7 @@
+import React from "react";
+import MissPassword from "../components/login-logout/MissPassword";
+function MissPassWordPage() {
+    return <MissPassword/>;
+}
+
+export default MissPassWordPage;
---
+++ pages/register.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Register from "../components/login-logout/Register";
+function RegisterPage() {
+    return <Register/>;
+}
+
+export default RegisterPage;
