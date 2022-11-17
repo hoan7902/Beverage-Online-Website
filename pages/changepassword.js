64f8224b8958439ab1b9854a1aa410dfd2a44330
giahuy2002 <78332589+giahuy2002@users.noreply.github.com>
2022-11-17 23:47:41 +0700

    Add page auth and misspassword

---
+++ pages/changepassword.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ChangePassWord from "../components/profile/ChangePassWord";
+function ChangePassPage() {
+    return <ChangePassWord/>;
+}
+
+export default ChangePassPage;
